3f3fa03d81e2c240a4c588cc2771a436cfd909d9
Adam Klein <agklein@gmail.com>
2022-05-21 17:52:33 -0600

    WIP: Make encounters moveable, make doors work

--- Assets/Scripts/Encounters/Encounter.cs
+++ Assets/Scripts/Encounters/Encounter.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public interface Encounter :  GameScene 
 {
 
-    /*
-        This function should probably return an EncounterInRoom interface in the future
-        so we can make Encounters look different based on their contents 
-    */
-    Vector2 getLocationInRoom();
     void setLocationInRoom(Vector2 loc);
     EncounterType getEncounterType();
     List<Enemy> getEnemies();
--- Assets/Scripts/Rooms/RoomFactories/DefaultRoomFactory.cs
+++ Assets/Scripts/Rooms/RoomFactories/DefaultRoomFactory.cs
@@ -8,21 +8,34 @@ public class DefaultRoomFactory
     public DefaultRoomFactory(){}
     public void generateRoom(DefaultRoom room)
     {
+
         RoomManager manager = GameObject.FindGameObjectWithTag("Managers").GetComponent<RoomManager>();
         manager.setActiveRoom(room);
-        GameObject encounterInRoomPrefab = GameObject.Find("PrefabStore").GetComponent<PrefabStore>().getPrefabByName("EncounterInRoom");
-        GameObject obj;
-        EncounterInRoom encounterInRoom;
-        foreach(Encounter encounter in room.getEncounters())
-        {
-            obj = Object.Instantiate(encounterInRoomPrefab, encounter.getLocationInRoom(), Quaternion.identity);
-            encounterInRoom = obj.GetComponent<EncounterInRoom>();
-            encounterInRoom.setEncounter(encounter);
-        }
+        
+        generateEncounters(room);
+        generatePlayer();
     }
 
     private void generatePlayer(){
         PlayerInRoomFactory playerInRoomFactory = new PlayerInRoomFactory();
         playerInRoomFactory.generatePlayer();
+    }
+
+    private void generateEncounters(DefaultRoom room){
+        GameObject encounterInRoomPrefab = GameObject.Find("PrefabStore").GetComponent<PrefabStore>().getPrefabByName("EncounterInRoom");
+        GameObject obj;
+        EncounterInRoom encounterInRoom;
+        LocationStore encounterStore = GameObject.FindGameObjectWithTag("EncounterStore").GetComponent<LocationStore>();
+        List<Encounter> encounters = room.getEncounters();
+        if(encounters.Count > encounterStore.getTopLevelCount()){
+            Debug.LogError("Not enough encounter locations in EncounterStore for the amount of encounters in scene: " + room.getSceneString());
+        }
+        for(int i = 0; i < encounters.Count; i += 1)
+        {
+            //still weighing whether we should have a factory method to call here...
+            obj = Object.Instantiate(encounterInRoomPrefab, encounterStore.getLoc(i), Quaternion.identity);
+            encounterInRoom = obj.GetComponent<EncounterInRoom>();
+            encounterInRoom.setEncounter(encounters[i]);
+        }
     }
 }
