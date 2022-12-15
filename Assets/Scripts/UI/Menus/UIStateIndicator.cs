2b2f0cdc26c49e179501316d5c36fbf49a2c98c6
Adam Klein <agklein@gmail.com>
2022-12-15 12:59:26 -0800

    Add Debug Texts, add text display of ui state

---
+++ Assets/Scripts/UI/Menus/UIStateIndicator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+[RequireComponent(typeof(UIStateEventListener))]
+[RequireComponent(typeof(EffectTargetRequestEventListener))]
+public class UIStateIndicator : MonoBehaviour
+{
+    private TextMeshProUGUI UIStateText;
+
+    void Start() {
+        UIStateText = GetComponent<TextMeshProUGUI>();
+    }
+
+
+    public void effectTargetRequestEventHandler(EffectTargetRequestEventInfo info) {
+        UIStateText.text = "Getting target for effect '" + info.effect.effectName + "', press Q to cancel";
+    }
+
+
+    public void UIStateEventHandler(UIStateEventInfo info){
+        switch(info.newState) {
+            case(UIState.EFFECT_TARGETTING):
+                // no op, handled by effect request
+                // UIStateText.text = "Getting target for card effect, press 'Q' to cancel";
+                break;
+            case(UIState.DEFAULT):
+                UIStateText.text = "UI in default state for turn phase";
+                break;
+        }
+    }
+
+}
