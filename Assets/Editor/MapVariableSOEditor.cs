6f50a497b0c8461f444a3cce87479437d6f73b6d
Adam-G-Klein <agklein98@gmail.com>
2023-04-11 21:18:33 -0700

    Fix merge to main issues

--- Assets/Editor/MapVariableSOEditor.cs
+++ Assets/Editor/MapVariableSOEditor.cs
@@ -1,24 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+using UnityEngine.SceneManagement;
 
-[CustomEditor(typeof(MapVariableSO))]
-public class MapVariableSOEditor : Editor {
+[System.Serializable]
+public class Map
+{
+    [SerializeReference]
+    public List<Encounter> encounters;
 
-    public override void OnInspectorGUI() {
-        MapVariableSO mapVariable= (MapVariableSO) target;
-        Map map = mapVariable.GetValue();
-        DrawDefaultInspector();
-        
-        EditorGUILayout.Space(20);
-        EditorGUILayout.LabelField("Test Utilities");
-        EditorGUILayout.Space(5);
-        
-        EditorGUILayout.LabelField("Map Completion: " + map.getCompletionPercentage() + "%");
+    public Map(List<Encounter> encounters) {
+        this.encounters = encounters;
+    }
 
-        if (GUILayout.Button("Reset Map Completion")) {
-            map.resetCompletion();
+    public void loadEncounterById(string id, EncounterVariableSO encounterVariable) {
+        Encounter encounterToLoad = getEncounterById(id);
+        if (encounterToLoad == null) {
+            // Make this actually do something
+            // SceneManager.LoadScene("End");
+            Debug.LogError("Cannot find encounter with given id " + id);
+            return;
         }
+
+        encounterVariable.SetValue(encounterToLoad);
+        switch (encounterToLoad.getEncounterType()) {
+            case EncounterType.Enemy:
+                SceneManager.LoadScene("PlaceholderEnemyEncounter");
+            break;
+
+            case EncounterType.Shop:
+                SceneManager.LoadScene("PlaceholderShopEncounter");
+            break;
+
+            default:
+                // Also make this do something
+                SceneManager.LoadScene("End");
+            break;
+        }
+    }
+
+    public void loadMapScene() {
+        SceneManager.LoadScene("Map");
+    }
+
+    public void loadNextEncounter(EncounterVariableSO activeEncounterVariable) {
+        Encounter nextEncounter = getNextEncounter(activeEncounterVariable.GetValue());
+        if (nextEncounter == null) {
+            // Make this actually do something
+            SceneManager.LoadScene("End");
+        }
+        activeEncounterVariable.SetValue(nextEncounter);
+        switch (nextEncounter.getEncounterType()) {
+            case EncounterType.Enemy:
+                SceneManager.LoadScene("PlaceholderEnemyEncounter");
+            break;
+
+            case EncounterType.Shop:
+                SceneManager.LoadScene("PlaceholderShopEncounter");
+            break;
+
+            default:
+                // Also make this do something
+                SceneManager.LoadScene("End");
+            break;
+        }
+    }
+
+    private Encounter getNextEncounter(Encounter activeEncounter) {
+        for(int i = 0; i < encounters.Count; i++) {
+            if (encounters[i].id == activeEncounter.id) {
+                if (i+1 == encounters.Count) {
+                    return null;
+                }
+                return encounters[i+1];
+            }
+        }
+        Debug.LogError("Active encounter not found in encounters list");
+        return null;
+    }
+
+    private Encounter getEncounterById(string id) {
+        foreach (Encounter encounter in this.encounters) {
+            if (encounter.id == id) {
+                return encounter;
+            }
+        }
+
+        return null;
     }
 }
