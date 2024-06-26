1112bd2a98f65e7c7e4525ef673badeba1f2701e
QuantumWombat <jlconant14@gmail.com>
2024-06-25 18:37:28 -0700

    Remove unused comment

--- Assets/Scripts/Decks/StartingDeck.cs
+++ Assets/Scripts/Decks/StartingDeck.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Not currently in use
 [CreateAssetMenu(
     fileName = "StartinDeck", 
     menuName = "Decks/Starting Deck")]
