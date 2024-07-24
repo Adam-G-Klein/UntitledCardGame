b96403073bd58ce30e5909ddca292491d1aca075
agklein <agklein@amazon.com>
2024-07-23 19:22:39 -0700

    Attempt to fix build errors, attempt to banish antique hunter font

--- Assets/Scripts/TestScripts/TestDealCards.cs
+++ Assets/Scripts/TestScripts/TestDealCards.cs
@@ -1,7 +1,6 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class TestDealCards : MonoBehaviour
--- Assets/Scripts/UI/UIDocumentControl/MiniUIDocumentWorldspace.cs
+++ Assets/Scripts/UI/UIDocumentControl/MiniUIDocumentWorldspace.cs
@@ -4,7 +4,6 @@ using UnityEngine.UIElements;
 using System.Collections;
 using UnityEditor;
 
-[InitializeOnLoad]
 [RequireComponent(typeof(UIDocument))]
 public class MiniUIDocumentWorldspace : MonoBehaviour {
 
