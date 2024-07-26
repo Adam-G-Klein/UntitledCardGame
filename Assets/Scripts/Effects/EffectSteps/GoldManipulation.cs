d32d5c3b6ba3ec7fd62bd607f2299f88928af155
QuantumWombat <jlconant14@gmail.com>
2024-07-25 19:07:32 -0700

    GoldManipulation

--- Assets/Scripts/Effects/EffectSteps/GoldManipulation.cs
+++ Assets/Scripts/Effects/EffectSteps/GoldManipulation.cs
@@ -6,12 +6,23 @@ public class GoldManipulation : EffectStep
     [SerializeField]
     public int goldToAdd = 0;
 
+    [SerializeField]
+    public bool getScaleFromKey = false;
+
+    [SerializeField]
+    public string inputScaleKey = "";
+
     public override IEnumerator invoke(EffectDocument document) {
+        int finalScale = goldToAdd;
+        if (getScaleFromKey && document.intMap.ContainsKey(inputScaleKey)) {
+            finalScale = document.intMap[inputScaleKey];
+        }
+
         PlayerData playerData = EnemyEncounterManager.Instance.gameState.playerData.GetValue();
 
-        Debug.Log("Giving the player $" + goldToAdd.ToString());
+        Debug.Log("Giving the player $" + finalScale.ToString());
 
-        playerData.gold += goldToAdd;
+        playerData.gold += finalScale;
 
         yield return null;
     }
