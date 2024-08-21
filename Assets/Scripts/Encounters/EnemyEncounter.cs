759ab7816283b2ccbdeb6d4de2f6af90128730a0
QuantumWombat <jlconant14@gmail.com>
2024-08-21 10:50:35 -0700

    Layout groups and constant sizes for enemies / companions
    This changes the enemies to be in a vertical layout group.
    It also gives each enemy and companion instance a LayoutElement component
    plus a ContentSizeFiter, so that we can keep a constant size.
    This should result in more visiblity in our game.

--- Assets/Scripts/Encounters/EnemyEncounter.cs
+++ Assets/Scripts/Encounters/EnemyEncounter.cs
@@ -7,11 +7,6 @@ public class EnemyEncounter : Encounter
 {
     public List<Enemy> enemyList = new List<Enemy>();
 
-    private EncounterConstantsSO encounterConstants;
-
-    private LocationStore companionLocationStore;
-    private LocationStore enemyLocationStore;
-
     public EnemyEncounter() {
         this.encounterType = EncounterType.Enemy;
     }
@@ -29,54 +24,4 @@ public class EnemyEncounter : Encounter
         encounterBuilder.BuildEnemyEncounter(this, encounterBuilder.companionLocationStore, encounterBuilder.enemyLocationStore);
     }
 
-    public void Build(
-            List<Companion> companionList,
-            EncounterConstantsSO constants,
-            List<CompanionInstance> createdCompanions,
-            List<EnemyInstance> createdEnemies,
-            LocationStore companionLocationStore,
-            LocationStore enemyLocationStore)
-    {
-        this.encounterType = EncounterType.Enemy;
-        this.encounterConstants = constants;
-        this.companionLocationStore = companionLocationStore;
-        this.enemyLocationStore = enemyLocationStore;
-        setupEnemies(createdEnemies);
-        // setupCompanions(companionList, createdCompanions);
-    }
-
-    private void setupEnemies(List<EnemyInstance> createdEnemies)
-    {
-        if (enemyList.Count > enemyLocationStore.getTopLevelCount()) {
-            Debug.LogError("The enemy locations list does not contain enough locations");
-            return;
-        }
-
-        for(int i = 0; i < enemyList.Count; i++)
-        {
-            createdEnemies.Add(PrefabInstantiator.instantiateEnemy(
-                encounterConstants.enemyPrefab,
-                enemyList[i],
-                enemyLocationStore.getLoc(i),
-                enemyLocationStore.transform));
-        }
-    }
-
-    private void setupCompanions(List<Companion> companionList, List<CompanionInstance> createdCompanions)
-    {
-        int activeCompanionsCount = companionList.Count;
-        if (activeCompanionsCount > companionLocationStore.getTopLevelCount()) {
-            Debug.LogError("The companion locations list does not contain enough locations");
-            return;
-        }
-
-        // for(int i = 0; i < activeCompanionsCount; i++)
-        // {
-        //     createdCompanions.Add(PrefabInstantiator.InstantiateCompanion(
-        //         encounterConstants.companionPrefab,
-        //         companionList[i],
-        //         companionLocationStore.getLoc(i),
-        //         companionLocationStore.transform));
-        // }
-    }
 }
