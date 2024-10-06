ee6bdbf6b8be2bfb251abd4e8eeb7a458faee21b
agklein <agklein@amazon.com>
2024-10-06 12:55:20 -0700

    Add statusEffectsSO

---
+++ Assets/Scripts/UI/Portraits/StatusEffectsSO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class StatusEffect {
+    public StatusEffectType type;
+    public Sprite image;
+
+    public StatusEffect() {}
+
+    public static bool operator== (StatusEffect a, StatusEffect b) {
+        return a.type == b.type;
+    }
+
+    public static bool operator!= (StatusEffect a, StatusEffect b) {
+        return a.type != b.type;
+    }
+}
+
+[CreateAssetMenu(fileName = "StatusEffectsSO", menuName = "StatusEffectsSO", order = 0)]
+public class StatusEffectsSO : ScriptableObject {
+
+    [SerializeField]
+    public List<StatusEffect> statusEffects;
+
+    public Sprite GetStatusEffectImage(StatusEffectType type) {
+        foreach (StatusEffect statusEffect in statusEffects) {
+            if (statusEffect.type == type) {
+                return statusEffect.image;
+            }
+        }
+        return null;
+    }
+    
+}
