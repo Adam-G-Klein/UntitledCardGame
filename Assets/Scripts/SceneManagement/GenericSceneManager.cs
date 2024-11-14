a66c7b78c000a3da3444d4ae865a5c03734e624e
LuckyLukeLeo <llawesome1@gmail.com>
2024-11-13 20:20:35 -0800

    Finish tweaking the intro cutscene

--- Assets/Scripts/SceneManagement/GenericSceneManager.cs
+++ Assets/Scripts/SceneManagement/GenericSceneManager.cs
@@ -8,12 +8,23 @@ public class GenericSceneManager : MonoBehaviour
 
     public bool allowProgressingGameState;
     public GameObject skipTooltip;
+    public int skipClicks = 3;
 
     private bool showingSkipTooltipAlready = false;
+    private bool checkingForSkipClicksAlready = false;
+
+    private int skipCounter = 0;
 
     public void Update() {
         if (allowProgressingGameState && Input.GetMouseButtonDown(0)) {
-            StartCoroutine(ShowSkipTooltip());
+            // StartCoroutine(ShowSkipTooltip());
+            if (!checkingForSkipClicksAlready) {
+                skipCounter = 1;
+                StartCoroutine(PossiblyShowSkipTooltip());
+                checkingForSkipClicksAlready = true;
+            } else {
+                skipCounter += 1;
+            }
         }
 
         if (allowProgressingGameState && Input.GetKeyDown(KeyCode.S)) {
@@ -23,6 +34,14 @@ public class GenericSceneManager : MonoBehaviour
 
     public void ContinueGameState() {
         gameState.LoadNextLocation();
+    }
+
+    private IEnumerator PossiblyShowSkipTooltip() {
+        yield return new WaitForSeconds(1f);
+        if (skipCounter >= skipClicks) {
+            StartCoroutine(ShowSkipTooltip());
+        }
+        checkingForSkipClicksAlready = false;
     }
 
     private IEnumerator ShowSkipTooltip() {
