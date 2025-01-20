91808c38209c0382a7a437915f3e318db4875859
QuantumWombat <jlconant14@gmail.com>
2025-01-20 18:05:17 +0100

    Initial progress on the entity ability refactor

---
+++ Assets/Scripts/Entity/EntityAbility.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EntityAbility
+{
+    [SerializeField]
+    public EntityAbilityTrigger abilityTrigger;
+    [SerializeReference]
+    public List<EffectStep> effectSteps;
+    public EffectWorkflow effectWorkflow { get {
+        return new EffectWorkflow(effectSteps);
+        }
+    }
+
+    public enum EntityAbilityTrigger {
+        EnterTheBattlefield,
+        EndOfCombat,
+        EndOfPlayerTurn,
+        EndOfEnemyTurn,
+        OnFriendOrFoeDeath,
+        OnFriendDeath,
+        OnDeath,
+        OnCardCast,
+        OnCombine,
+        OnCardExhausted,
+        OnDeckShuffled,
+        OnFriendDamageTaken,
+        StartOfPlayerTurn,
+    }
+}
