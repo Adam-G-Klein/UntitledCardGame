ea16e7cba7c124e3bf27cc5d64c83983f755ab72
agklein <agklein@amazon.com>
2025-02-07 21:31:42 -0800

    Add options menu accessible from combat and shop, add exit buttons to it

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -7,6 +7,8 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject optionsUIPrefab;
+    [SerializeField]
+    private GameStateVariableSO gameState;
 
     public void startButtonHandler() {
         SceneManager.LoadScene("GenerateMap");
