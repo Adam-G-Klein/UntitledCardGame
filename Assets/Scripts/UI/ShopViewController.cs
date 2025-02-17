bb19fe03d134d0ab80173a6bff440fe445dbc75b
agklein <agklein@amazon.com>
2025-02-16 18:55:13 -0800

    Make webgl build work :)

--- Assets/Scripts/UI/ShopViewController.cs
+++ Assets/Scripts/UI/ShopViewController.cs
@@ -2,10 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UIElements;
 
