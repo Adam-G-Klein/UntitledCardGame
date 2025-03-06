9279e30b6edd7aca92a6856f9282dd7aec2f67f4
ethan-tucker <ethan.tucker.99@gmail.com>
2025-03-06 15:18:51 -0800

    Updating box collider hitbox to prevent endless hovering/unhovering

--- Assets/Scripts/UI/UIDocumentControl/UIDocumentCard.cs
+++ Assets/Scripts/UI/UIDocumentControl/UIDocumentCard.cs
@@ -23,6 +23,7 @@ public class UIDocumentCard : MonoBehaviour
     private Texture2D texture { get; set; }
     public static Vector2Int CARD_REFERENCE_RESOLUTION = new(400, 700);
     public Vector2 CARD_SIZE = new Vector2(1f, 1.75f);
+    public Vector2 HITBOX_SIZE = new Vector2(1f, 2.4f); // this helps if the cards are along the bottom of the screen to prevent constant hovering/unhovering
 
     [Header("Only for dev, one of the worst possible things you could do for GPU performance")]
     public bool renderTextureConstantly = false;
@@ -66,7 +67,7 @@ public class UIDocumentCard : MonoBehaviour
         UIDocumentUtils.SetAllPickingMode(doc.rootVisualElement, PickingMode.Ignore);
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.size = CARD_SIZE;
-        boxCollider.size = CARD_SIZE;
+        boxCollider.size = HITBOX_SIZE;
         pCard.UpdateCardText(); //we gonna run this shit like a millino times
         spriteRenderer.material.SetTexture("_SecondTex", doc.panelSettings.targetTexture);
     }
