42dfce5ce33f6b4af0bd92c8d6291f1517984ea4
QuantumWombat <jlconant14@gmail.com>
2025-04-28 14:23:35 +0200

    Ok, start working on new pythia version

--- Assets/Scripts/Effects/EffectSteps/CombatInstanceCacheStore.cs
+++ Assets/Scripts/Effects/EffectSteps/CombatInstanceCacheStore.cs
@@ -69,9 +69,11 @@ public class CombatInstanceCacheStore : EffectStep
             }
             if (isBool) {
                 target.cachedEffectValues.boolMap[cacheKey] = document.boolMap[currentWorkflowKey];
+                Debug.Log("[CombatInstanceCacheStore] Key '" + cacheKey + "' updated BOOL value " + target.cachedEffectValues.boolMap[cacheKey]);
             }
             if (isInt) {
                 target.cachedEffectValues.intMap[cacheKey] = document.intMap[currentWorkflowKey];
+                Debug.Log("[CombatInstanceCacheStore] Key '" + cacheKey + "' updated INT value " + target.cachedEffectValues.intMap[cacheKey]);
             }
             yield break;
         }
