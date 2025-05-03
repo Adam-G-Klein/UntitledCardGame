820dd10ea9f8d58128734bb501a588fe4bc27544
ethan-tucker <ethan.tucker.99@gmail.com>
2025-05-03 11:46:30 -0700

    Preventing these cards from having unecessary green highlight

--- Assets/Scripts/Effects/EffectSteps/EndWorkflowIfListEmpty.cs
+++ Assets/Scripts/Effects/EffectSteps/EndWorkflowIfListEmpty.cs
@@ -25,7 +25,6 @@ public class EndWorkflowIfListEmpty : EffectStep, IEffectStepCalculation {
         if (interupt) {
             document.workflowInterrupted = true;
         }
-        document.boolMap["highlightCard"] = !interupt;
         yield return null;
     }
 
@@ -35,7 +34,6 @@ public class EndWorkflowIfListEmpty : EffectStep, IEffectStepCalculation {
         if (interupt) {
             document.workflowInterrupted = true;
         }
-        document.boolMap["highlightCard"] = !interupt;
         yield return null;
     }
 
