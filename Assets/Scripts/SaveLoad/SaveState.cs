35b2d7638c7c60115cabcf104a68d5c284ff5656
agklein <agklein@amazon.com>
2025-07-01 19:57:34 -0700

    Companions serialize load with their companion types. Still TODOs obv

--- Assets/Scripts/SaveLoad/SaveState.cs
+++ Assets/Scripts/SaveLoad/SaveState.cs
@@ -9,35 +9,35 @@ public class SaveState {
 
     private string saveName;
     public string SaveName => saveName;
-    // private List<CompanionSerializeable> activeCompanions;
-    // private List<CompanionSerializeable> benchCompanions;
-    // private List<EncounterSerializeable> map;
+    private List<CompanionSerializable> activeCompanions;
+    // private List<CompanionSerializable> benchCompanions;
+    // private List<EncounterSerializable> map;
     // private int currentEncounterIndex;
-    // private PlayerDataSerializeable playerData;
+    // private PlayerDataSerializable playerData;
 
     public SaveState(string saveName, GameStateVariableSO gameState) {
         this.saveName = saveName;
-        // this.activeCompanions = gameState.companions.activeCompanions
-        //     .Select(companion => new CompanionSerializeable(companion))
-        //     .ToList();
+        this.activeCompanions = gameState.companions.activeCompanions
+             .Select(companion => new CompanionSerializable(companion))
+             .ToList();
         
         // this.benchCompanions = gameState.companions.benchedCompanions
-        //     .Select(companion => new CompanionSerializeable(companion))
+        //     .Select(companion => new CompanionSerializable(companion))
         //     .ToList();
         
         // this.map = gameState.map.GetValue().encounters
-        //     .Select(encounter => new EncounterSerializeable(encounter))
+        //     .Select(encounter => new EncounterSerializable(encounter))
         //     .ToList();
         
         // this.currentEncounterIndex = gameState.currentEncounterIndex;
 
-        // this.playerData = new PlayerDataSerializeable(gameState.playerData);
+        // this.playerData = new PlayerDataSerializable(gameState.playerData);
     }
 
     public void LoadToGameState(GameStateVariableSO gameState, SORegistry registry) {
-        // gameState.companions.activeCompanions = this.activeCompanions
-        //     .Select(companion => new Companion(companion, registry))
-        //     .ToList();
+        gameState.companions.activeCompanions = this.activeCompanions
+             .Select(companion => new Companion(companion, registry))
+             .ToList();
         
         // gameState.companions.benchedCompanions = this.benchCompanions
         //     .Select(companion => new Companion(companion, registry))
