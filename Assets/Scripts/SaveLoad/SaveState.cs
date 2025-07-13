6fb3b914189df6b0da2d8f32d65a6977ea22afb4
ethan-tucker <ethan.tucker.99@gmail.com>
2025-07-13 11:58:59 -0700

    saving selected ascension level in saveState

--- Assets/Scripts/SaveLoad/SaveState.cs
+++ Assets/Scripts/SaveLoad/SaveState.cs
@@ -14,41 +14,46 @@ public class SaveState {
     private List<EncounterSerializable> map;
     private int currentEncounterIndex;
     private PlayerDataSerializable playerData;
+    private int ascensionLevel;
 
-    public SaveState(string saveName, GameStateVariableSO gameState) {
-        this.saveName = saveName;
-        this.activeCompanions = gameState.companions.activeCompanions
-             .Select(companion => new CompanionSerializable(companion))
-             .ToList();
+     public SaveState(string saveName, GameStateVariableSO gameState)
+     {
+          this.saveName = saveName;
+          this.activeCompanions = gameState.companions.activeCompanions
+               .Select(companion => new CompanionSerializable(companion))
+               .ToList();
 
-        this.benchCompanions = gameState.companions.benchedCompanions
-             .Select(companion => new CompanionSerializable(companion))
-             .ToList();
+          this.benchCompanions = gameState.companions.benchedCompanions
+               .Select(companion => new CompanionSerializable(companion))
+               .ToList();
 
-        this.map = gameState.map.GetValue().encounters
-             .Select<Encounter, EncounterSerializable>(encounter => encounter.getEncounterType() == EncounterType.Enemy ?
-                new EnemyEncounterSerializable((EnemyEncounter)encounter)
-                : new ShopEncounterSerializable((ShopEncounter)encounter))
-             .ToList();
-        
-        this.currentEncounterIndex = gameState.currentEncounterIndex;
+          this.map = gameState.map.GetValue().encounters
+               .Select<Encounter, EncounterSerializable>(encounter => encounter.getEncounterType() == EncounterType.Enemy ?
+                  new EnemyEncounterSerializable((EnemyEncounter)encounter)
+                  : new ShopEncounterSerializable((ShopEncounter)encounter))
+               .ToList();
 
-        this.playerData = new PlayerDataSerializable(gameState.playerData.GetValue());
-    }
+          this.currentEncounterIndex = gameState.currentEncounterIndex;
 
-    public void LoadToGameState(GameStateVariableSO gameState, SORegistry registry) {
-        gameState.companions.activeCompanions = this.activeCompanions
-             .Select(companion => new Companion(companion, registry))
-             .ToList();
-        
-        gameState.companions.benchedCompanions = this.benchCompanions
-             .Select(companion => new Companion(companion, registry))
-             .ToList();
-        
-        gameState.map.SetValue(new Map(this.map, registry, gameState.baseShopData));
+          this.playerData = new PlayerDataSerializable(gameState.playerData.GetValue());
+          this.ascensionLevel = gameState.ascensionLevel;
+     }
 
-        gameState.currentEncounterIndex = this.currentEncounterIndex;
+     public void LoadToGameState(GameStateVariableSO gameState, SORegistry registry)
+     {
+          gameState.companions.activeCompanions = this.activeCompanions
+               .Select(companion => new Companion(companion, registry))
+               .ToList();
 
-        gameState.playerData.SetValue(new PlayerData(this.playerData));
+          gameState.companions.benchedCompanions = this.benchCompanions
+               .Select(companion => new Companion(companion, registry))
+               .ToList();
+
+          gameState.map.SetValue(new Map(this.map, registry, gameState.baseShopData));
+
+          gameState.currentEncounterIndex = this.currentEncounterIndex;
+
+          gameState.playerData.SetValue(new PlayerData(this.playerData));
+          gameState.ascensionLevel = this.ascensionLevel;
     }
 }
