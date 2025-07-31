bc1134c199b5b377a621a95ae4f5d427c38c1a3b
ethan-tucker <ethan.tucker.99@gmail.com>
2025-07-31 10:19:47 -0700

    Simplifying tutorial text across the board to speed up onboarding

--- Assets/Scripts/Tutorial/TutorialActions/UpdateTutorialUIDocAction.cs
+++ Assets/Scripts/Tutorial/TutorialActions/UpdateTutorialUIDocAction.cs
@@ -9,10 +9,10 @@ public class UpdateTutorialUIDocAction : TutorialAction
 {
     [SerializeField]
     private string firstLineOfText;
-    
+
     [SerializeField]
     private string secondLineOfText;
-    
+
     [SerializeField]
     private string thirdLineOfText;
 
@@ -30,31 +30,25 @@ public class UpdateTutorialUIDocAction : TutorialAction
     [SerializeField]
     private Sprite tutorialImage;
 
-    public UpdateTutorialUIDocAction() {
+    public UpdateTutorialUIDocAction()
+    {
         tutorialActionType = "Update Tutorial UI Doc Action";
     }
 
-    public override IEnumerator Invoke() {
-        Debug.Log("updating tutorial doc");
-        // update the text in the UI doc...can I even do that?
+    public override IEnumerator Invoke()
+    {
         var root = uiDocument.rootVisualElement;
-        Label text1 = root.Q<Label>("tutorialText1");
-        text1.text = firstLineOfText;
-        //text1.MarkDirtyRepaint();
-        Label text2 = root.Q<Label>("tutorialText2");
-        text2.text = secondLineOfText;
-        //text2.MarkDirtyRepaint();
-        Label text3 = root.Q<Label>("tutorialText3");
-        text3.text = thirdLineOfText; 
-        //text3.MarkDirtyRepaint();
-        //TODO: force the document to rerender or something like that I think
+        UpdateTutorialText(root.Q<Label>("tutorialText1"), firstLineOfText);
+        UpdateTutorialText(root.Q<Label>("tutorialText2"), secondLineOfText);
+        UpdateTutorialText(root.Q<Label>("tutorialText3"), thirdLineOfText);
         Label tutorialIndexText = root.Q<Label>("tutorialIndexText");
-        tutorialIndexText.text = $"(Page {tutorialIndex} of {tutorialStepsCount})"; 
+        tutorialIndexText.text = $"(Page {tutorialIndex} of {tutorialStepsCount})";
 
         VisualElement visualElement = root.Q<VisualElement>("tutorialImage");
-        visualElement.style.backgroundImage = new StyleBackground(tutorialImage); 
+        visualElement.style.backgroundImage = new StyleBackground(tutorialImage);
 
-        if (buttonText != null) {
+        if (buttonText != null)
+        {
             Button tutorialButton = root.Q<Button>("next");
             tutorialButton.text = buttonText;
         }
@@ -64,8 +58,18 @@ public class UpdateTutorialUIDocAction : TutorialAction
         //text1.style.display = DisplayStyle.None;
         //text1.style.display = DisplayStyle.Flex;
 
-        Debug.Log($"Text was set to: {text1.text}");
-
         yield return null;
+    }
+
+    public void UpdateTutorialText(Label label, String text) {
+        if (String.IsNullOrEmpty(text.Trim()))
+        {
+            label.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            label.style.display = DisplayStyle.Flex;
+            label.text = text;
+        }
     }
 }
