cbae9d1af170ac5d6199ecdc9d3ed27b73b38f53
QuantumWombat <jlconant14@gmail.com>
2025-11-14 11:56:23 +0100

    Priorityeventdispatcher.cs

---
+++ Assets/Scripts/Utils/PriorityEventDispatcher.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PriorityDispatcher<TDelegate> where TDelegate : Delegate
+{
+    private readonly List<(int priority, TDelegate handler)> handlers = new();
+
+    public void AddHandler(TDelegate handler, int priority)
+    {
+        handlers.Add((priority, handler));
+        handlers.Sort((a, b) => a.priority.CompareTo(b.priority));
+    }
+
+    public void RemoveHandler(TDelegate handler)
+    {
+        handlers.RemoveAll(h => h.handler == handler);
+    }
+
+    public IEnumerable<IEnumerator> Invoke(params object[] args)
+    {
+        foreach (var (priority, handler) in handlers)
+        {
+            // handler.DynamicInvoke(args) returns an object; cast to IEnumerator
+            yield return (IEnumerator)handler.DynamicInvoke(args);
+        }
+    }
+}
